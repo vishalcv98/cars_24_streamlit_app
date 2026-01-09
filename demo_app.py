5e86cb0a9b8c46af6eefd16f5a36c3295499d4cf
Vishal <vishalcv@gmail.com>
2026-01-09 20:20:08 +0530

    test

--- demo_app.py
+++ demo_app.py
@@ -9,4 +9,3 @@ num = st.number_input("Enter a number", value=0)
 if st.button("Calculate the square"):
     result = sqr(num)
     st.write(f"The square of {num} is {result}")
-
--- test_model.py
+++ test_model.py
@@ -3,11 +3,12 @@ import pickle
 import pandas as pd
 import numpy as np
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session") # Everytime i run pytest, this model will be loaded only once
 def model():
     with open("xgb_car_price_model.pkl", "rb") as f:
         return pickle.load(f)
 
+# Makes any input model ready to be passed to the model for prediction
 def make_input(
     km_driven,
     mileage,
@@ -30,11 +31,34 @@ def make_input(
 def test_prediction_reasonable_range_multiple_samples(model):
     for _ in range(100):   # test 100 random combinations
         X = make_input(
-            mileage=np.random.randint(1000, 200000),
+            mileage=np.random.randint(5, 20),
             age=np.random.randint(0, 20),
             km_driven=np.random.randint(5000, 300000),
             fuel_type=np.random.choice(["Petrol", "Diesel", "Electric"])
         )
 
         price = model.predict(X)[0]
-        assert -100 < price < 500
+        if price<0:
+            price=0
+        assert 0 <= price <= 100
+
+def test_age_pred_compare(model):
+     
+    X1 = make_input(
+            mileage=10,
+            age=5,
+            km_driven=20000,
+            fuel_type="Petrol"
+        )
+    X2 = make_input(
+            mileage=10,
+            age=15,
+            km_driven=20000,
+            fuel_type="Petrol"
+        )
+
+    price1 = model.predict(X1)[0]
+    price2 = model.predict(X2)[0]
+
+
+    assert price1>price2
